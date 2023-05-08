5b31afc3009d3f233256a1b3186d32327f4b7edc
Clemens Weber <jan.clemens.weber-2@mni.thm.de>
2023-05-08 18:29:26 +0200

    Sorting

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -4,14 +4,42 @@ import {AddTodoComponent} from "../add-todo/add-todo.component";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {TodoService} from "../todo.service";
 
+let bool : boolean = true;
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
+
 export class ListComponent {
   @Input() list!: ToDoEntry[];
 
   constructor(private modalService: NgbModal, public todoService: TodoService) {
+  }
+
+  sort(toSort: string){
+    switch(toSort){
+        case 'title':
+          console.log(bool);
+          if(bool){
+            this.todoService.todoList.sort((a, b) => a.title < b.title ? -1 : 1);
+            bool = false;
+          } else {
+            this.todoService.todoList.sort((a, b) => a.title > b.title ? -1 : 1);
+            bool = true;
+          }
+          break;
+        case 'date':  
+        console.log(bool);
+        if(bool){
+          this.todoService.todoList.sort((a, b) => a.date < b.date ? -1 : 1);
+          bool = false;
+        } else {
+          this.todoService.todoList.sort((a, b) => a.date > b.date ? -1 : 1);
+          bool = true;
+        }
+          break;
+    }
   }
 }
