790726c004ad5d734a1288f258e0d31d4b5e9fcf
Clemens Weber <jan.clemens.weber-2@mni.thm.de>
2023-05-22 14:12:20 +0200

    Disable ESLint for this Line, because any is needed here

--- src/app/ToDoEntry.ts
+++ src/app/ToDoEntry.ts
@@ -6,6 +6,7 @@ export class ToDoEntry {
     date: Date
     priority: Priority
     kategorie?: string;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     [key: string]: any
 
     constructor(title: string, priority: Priority = Priority.Normal, kategorie?: string) {
